a9f210d2e641a8719f28c0b26556fac0c6e5f1f3
Josh <joshcamas@gmail.com>
2018-10-30 13:42:30 -0700

    initial build of bot

---
+++ index.js
@@ -0,0 +1,59 @@
+var Twit = require(‘twit’);
+var TwitterBot = require(‘node-twitterbot’).TwitterBot;
+
+var TWITTER_SEARCH_PHRASE = '#ardenfall';
+
+var Bot = new TwitterBot({
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+console.log('The bot is running...');
+
+/* BotInit() : To initiate the bot */
+function BotInit() {
+	Bot.post('statuses/retweet/:id', { id: '669520341815836672' }, BotInitiated);
+	
+	function BotInitiated (error, data, response) {
+		if (error) {
+			console.log('Bot could not be initiated, : ' + error);
+		}
+		else {
+  			console.log('Bot initiated : 669520341815836672');
+		}
+	}
+
+	var query = {
+		q: TWITTER_SEARCH_PHRASE,
+		result_type: "recent"
+	}
+
+	Bot.get('search/tweets', query, BotGotLatestTweet);
+
+	function BotGotLatestTweet (error, data, response) {
+		if (error) {
+			console.log('Bot could not find latest tweet, : ' + error);
+		}
+		else {
+			var id = {
+				id : data.statuses[0].id_str
+			}
+
+			Bot.post('statuses/retweet/:id', id, BotRetweeted);
+			
+			function BotRetweeted(error, response) {
+				if (error) {
+					console.log('Bot could not retweet, : ' + error);
+				}
+				else {
+					console.log('Bot retweeted : ' + id.id);
+				}
+			}
+		}
+	}
+}
+
+/* Initiate the Bot */
+BotInit();
